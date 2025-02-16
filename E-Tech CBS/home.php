32fd7a371b169292db668829e870f757e0c2c878
19niel <kungfayla@gmail.com>
2025-02-16 20:37:05 +0800

    merged

--- E-Tech CBS/home.php
+++ E-Tech CBS/home.php
@@ -1,6 +1,6 @@
-<?php 
+<!-- <?php 
     include 'components/connection.php';
-?>
+?> -->
 
 <style type="text/css"> 
     <?php include 'style.css'; ?>        
@@ -11,13 +11,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel="stylesheet"> -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel="stylesheet">
     <title>Green Coffee - home page</title>
 </head>
 <body>
-<?php //include 'components/header.php'; ?>        
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script> -->
+<?php include 'components/header.php'; ?>        
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script> 
     <script src="script.js"></script>
-    <?php //include 'components/alert.php'; ?>        
+    <?php include 'components/alert.php'; ?>        
 </body>
 </html>
